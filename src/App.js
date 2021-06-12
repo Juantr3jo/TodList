c09541a7a83d3fd506ed03293ef6478b8c62496a
Juantr3jo <jmtm89ve@gmail.com>
2021-06-11 21:03:22 -0400

    todolist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from "react"
+
+
+
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [thing, setThing] = useState("");
+
+
+  const change = (e) => {
+    e.preventDefault();
+    setThing(e.target.value)
+    
+  }
+  const add = () => {
+    setTodo([...todo, thing])
+  }
+
+  const deleteTask = i => {
+    console.log("borrando " + i);
+    todo.splice(i, 1);
+    setTodo([...todo]);
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+
+
+
+
+
+      <div className="input-group mt-4 w-50 m-auto">
+
+        <button onClick={add} className="btn btn-outline-secondary" type="button" id="button-addon1">Agregar</button>
+
+        <input onChange={change} type="text" className="form-control " placeholder="" aria-label="Agregar a la lista" aria-describedby="button-addon1" />
+        <h1>Todo List</h1>
+
+      </div>
+      <div className='col-6 m-auto'>
+        <button onClick={deleteTask} className="btn btn-outline-danger">Eliminar</button>
+      </div>
+      <ul>{todo.map((item, i) => (
+        <li>
+          <h3 key={i}> {item}</h3>
+        </li>
+      ))}</ul>
+
+
+
+
+    </>
   );
 }
 
